f18d5952b050136ac92b891faf9b64404114b0ca
nat <nattha.int@gmail.com>
2023-07-01 19:20:01 +0700

    feat: add create deployment endpoint

--- pages/api/v1/deployments.ts
+++ pages/api/v1/deployments.ts
@@ -1,5 +1,8 @@
 import {NextApiRequest, NextApiResponse} from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if(req.method === "POST") {
+    res.status(201).json({})
+  }
   res.status(200).json({ text: "hello" })
 }
