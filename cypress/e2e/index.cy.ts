e2f521dd41d341da8814c5e0484e6c705f4ed517
nat <nattha.int@gmail.com>
2023-07-02 15:20:41 +0700

    test: add login button

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -8,5 +8,9 @@ describe("test index", () => {
       cy.document().its("contentType").should("eq", "text/html");
       cy.contains("Home page");
     });
+
+    it("should render login button", () => {
+      cy.findByRole("button", { name: "Login" }).should("exist");
+    });
   });
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,10 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div>
+      Home page
+      <button>Login</button>
+    </div>
+  );
 }
